77eb82a900c91d4c719945996b826bf55cebc30f
Kate Ergo <KateErgo@acknowledge.nl>
2024-11-26 08:50:48 +0100

    updates and new config file

---
+++ configuration.js
@@ -0,0 +1,16 @@
+// configuration.js
+
+// How to use: 
+// -----------
+// This file contains the fetch URLS. Use local base URL when testing locally, use the remote base URL to test online.
+// In the html-files, configuration.js is called in the variable fetchUrls[0].
+
+
+// Fetch URLs (without endpoints)
+const fetchUrls = [
+    // Uncomment the next line to use the local URL:
+    //"http://127.0.0.1:5000", // Local base URL
+
+    // Uncomment the next line to use the remote Azure URL:
+    "https://damen2411-elony-backend-enaaeee5gze4cdeg.westeurope-01.azurewebsites.net" // Remote base URL
+];
